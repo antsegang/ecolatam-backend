1cffc9d4efcb53dd23e59a2b45c9586a6f6fbd42
Anthony Bernal Segura Angulo <absegura12@gmail.com>
2025-08-25 00:42:33 -0600

    Fix src/config.js conflicts

--- src/config.js
+++ src/config.js
@@ -13,17 +13,10 @@ export const config = {
     secret: process.env.JWT_SECRET || null,
   },
   mysql: {
-<<<<<<< ours
-    host: process.env.MYSQL_HOST || [YOUR_MYSQL_HOST],
-    user: process.env.MYSQL_USER || [YOUR_MYSQL_USER],
-    password: process.env.MYSQL_PASSWORD || [YOUR_MYSQL_PASSWORD],
-    database: process.env.MYSQL_DB || [YOUR_MYSQL_DB],
-=======
     host: process.env.MYSQL_HOST || "localhost",
     user: process.env.MYSQL_USER || "root",
     // No default password for security reasons
     password: process.env.MYSQL_PASSWORD || null,
     database: process.env.MYSQL_DB || null,
->>>>>>> theirs
   },
 };
