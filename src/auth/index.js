f2fb8582b4f45de976c352b98af16711f0745878
Anthony Bernal Segura Angulo <absegura12@gmail.com>
2025-08-24 23:46:25 -0600

    Rename error factory to createError and refactoring

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import { config } from "../config.js";
-import { error } from "../middlewares/errors.js";
+import { error as createError } from "../middlewares/errors.js";
 import db from "./../DB/mysql.js";
 
 const secret = config.jwt.secret;
@@ -13,144 +13,92 @@ function verifyToken(token) {
   return jwt.verify(token, secret);
 }
 
+async function isUserInTable(userId, table) {
+  const data = await db.query1(`SELECT * FROM ${table}`);
+  return data.some((item) => item.id_user === userId);
+}
+
+async function verifyOwnership(req, id, table) {
+  const { id: userId } = decodifyHeader(req);
+  const data = await db.query(`SELECT * FROM ${table} WHERE ?`, { id });
+  const ownerId = data[0].id_user;
+  if (ownerId !== userId) {
+    throw createError("No tienes privilegios para hacer esto", 401);
+  }
+}
+
 const checkAuth = {
   confirmToken: function (req, id) {
     const decodified = decodifyHeader(req);
 
     if (decodified.id !== id) {
-      throw error("No tienes privilegios para hacer esto", 401);
+      throw createError("No tienes privilegios para hacer esto", 401);
     }
   },
 };
 
 const checkSession = {
-  confirmToken: function (req, id) {
-    const decodified = decodifyHeader(req);
+  confirmToken: function (req) {
+    decodifyHeader(req);
   },
 };
 
 const checkAdmin = {
   confirmToken: async function (req, id) {
     const decodified = decodifyHeader(req);
-    const query = `SELECT * FROM admin`;
-    try {
-      const data = await db.query1(query);
-      const idUsersAdmin = [];
-      for (const item of data) {
-        const id_user = item.id_user;
-        idUsersAdmin.push(id_user);
-      }
-      if (!idUsersAdmin.includes(decodified.id)) {
-        checkSAdmin.confirmToken(req, id);
-      }
-      if (decodified.id !== id) {
-        throw error("No tienes privilegios para hacer esto", 401);
-      }
-    } catch (error) {
-      throw error;
+    const isAdmin = await isUserInTable(decodified.id, "admin");
+    if (!isAdmin) {
+      await checkSAdmin.confirmToken(req, id);
+      return;
+    }
+    if (decodified.id !== id) {
+      throw createError("No tienes privilegios para hacer esto", 401);
     }
   },
 };
 
 const checkCSAgent = {
   confirmToken: async function (req, id) {
-    const decodified = decodifyHeader(req);
-    const query = `SELECT * FROM csagent WHERE ?`;
-    try {
-      const data = await db.query(query, { id: id });
-      const idUserOwner = data[0].id_user;
-      if (idUserOwner !== decodified.id) {
-        throw error("No tienes privilegios para hacer esto", 401);
-      }
-      checkAuth.confirmToken(req, idUserOwner);
-    } catch (error) {
-      throw error;
-    }
+    await verifyOwnership(req, id, "csagent");
   },
 };
 
 const checkSAdmin = {
   confirmToken: async function (req, id) {
     const decodified = decodifyHeader(req);
-    const query = `SELECT * FROM superadmin`;
-    try {
-      const data = await db.query1(query);
-      const idUsersSAdmin = [];
-      for (const item of data) {
-        const id_user = item.id_user;
-        idUsersSAdmin.push(id_user);
-      }
-      if (!idUsersSAdmin.includes(decodified.id)) {
-        throw error("No tienes privilegios para hacer esto", 401);
-      }
-      if (decodified.id !== id) {
-        throw error("No tienes privilegios para hacer esto", 401);
-      }
-    } catch (error) {
-      throw error;
+    const isSAdmin = await isUserInTable(decodified.id, "superadmin");
+    if (!isSAdmin || decodified.id !== id) {
+      throw createError("No tienes privilegios para hacer esto", 401);
     }
   },
 };
 
 const checkOwner = {
   confirmToken: async function (req, id) {
-    const decodified = decodifyHeader(req);
-    const query = `SELECT * FROM business WHERE ?`;
-    try {
-      const data = await db.query(query, { id: id });
-      const idUserOwner = data[0].id_user;
-      if (idUserOwner !== decodified.id) {
-        throw error("No tienes privilegios para hacer esto", 401);
-      }
-      checkAuth.confirmToken(req, idUserOwner);
-    } catch (error) {
-      throw error;
-    }
+    await verifyOwnership(req, id, "business");
   },
 };
 
 const checkVolunteer = {
   confirmToken: async function (req, id) {
-    const decodified = decodifyHeader(req);
-    const query = `SELECT * FROM volunteer WHERE ?`;
-    try {
-      const data = await db.query(query, { id: id });
-      const idUserVolunteer = data[0].id_user;
-      if (idUserVolunteer !== decodified.id) {
-        throw error("No tienes privilegios para hacer esto", 401);
-      }
-      checkAuth.confirmToken(req, idUserVolunteer);
-    } catch (error) {
-      throw error;
-    }
+    await verifyOwnership(req, id, "volunteer");
   },
 };
 
 const checkKYCUser = {
   confirmToken: async function (req, id) {
     const decodified = decodifyHeader(req);
-    const query = `SELECT * FROM ukyc`;
-    try {
-      const data = await db.query1(query);
-      const idUsersSAdmin = [];
-      for (const item of data) {
-        const id_user = item.id_user;
-        idUsersSAdmin.push(id_user);
-      }
-      if (!idUsersSAdmin.includes(decodified.id)) {
-        throw error("No haz completado el KYC", 401);
-      }
-      if (decodified.id !== id) {
-        throw error("No tienes privilegios para hacer esto", 401);
-      }
-      const query2 = `SELECT * FROM ukyc WHERE id_user = ?`;
-      const data2 = await db.query(query2, id);
-      const kyc = data2[0];
-      if (kyc.approve !== 1) {
-        throw error("Tu verificación de identidad sigue en revisión", 401);
-      }
-    } catch (error) {
-      throw error;
+    const inKYC = await isUserInTable(decodified.id, "ukyc");
+    if (!inKYC) {
+      throw createError("No haz completado el KYC", 401);
+    }
+    if (decodified.id !== id) {
+      throw createError("No tienes privilegios para hacer esto", 401);
+    }
+    const data = await db.query("SELECT * FROM ukyc WHERE id_user = ?", id);
+    const kyc = data[0];
+    if (kyc.approve !== 1) {
+      throw createError("Tu verificación de identidad sigue en revisión", 401);
     }
   },
 };
@@ -158,73 +106,40 @@ const checkKYCUser = {
 const checkKYCBusiness = {
   confirmToken: async function (req, id) {
     const decodified = decodifyHeader(req);
-    const query = `SELECT * FROM bkyc`;
-    try {
-      const data = await db.query1(query);
-      const idUsersSAdmin = [];
-      for (const item of data) {
-        const id_user = item.id_user;
-        idUsersSAdmin.push(id_user);
-      }
-      if (!idUsersSAdmin.includes(decodified.id)) {
-        throw error("No haz completado el KYC", 401);
-      }
-      if (decodified.id !== id) {
-        throw error("No tienes privilegios para hacer esto", 401);
-      }
-      const query2 = `SELECT * FROM bkyc WHERE id_user = ?`;
-      const data2 = await db.query(query2, id);
-      const kyc = data2[0];
-      if (kyc.approve !== true) {
-        throw error("Tu verificación de identidad sigue en revisión", 401);
-      }
-    } catch (error) {
-      throw error;
+    const inKYC = await isUserInTable(decodified.id, "bkyc");
+    if (!inKYC) {
+      throw createError("No haz completado el KYC", 401);
+    }
+    if (decodified.id !== id) {
+      throw createError("No tienes privilegios para hacer esto", 401);
+    }
+    const data = await db.query("SELECT * FROM bkyc WHERE id_user = ?", id);
+    const kyc = data[0];
+    if (kyc.approve !== true) {
+      throw createError("Tu verificación de identidad sigue en revisión", 401);
     }
   },
 };
 
 const checkInspector = {
   confirmToken: async function (req, id) {
-    const decodified = decodifyHeader(req);
-    const query = `SELECT * FROM inspector WHERE ?`;
-    try {
-      const data = await db.query(query, { id: id });
-      const idUserOwner = data[0].id_user;
-      if (idUserOwner !== decodified.id) {
-        throw error("No tienes privilegios para hacer esto", 401);
-      }
-      checkAuth.confirmToken(req, idUserOwner);
-    } catch (error) {
-      throw error;
-    }
+    await verifyOwnership(req, id, "inspector");
   },
 };
 
 const checkTGuide = {
   confirmToken: async function (req, id) {
-    const decodified = decodifyHeader(req);
-    const query = `SELECT * FROM tour_guide WHERE ?`;
-    try {
-      const data = await db.query(query, { id: id });
-      const idUserOwner = data[0].id_user;
-      if (idUserOwner !== decodified.id) {
-        throw error("No tienes privilegios para hacer esto", 401);
-      }
-      checkAuth.confirmToken(req, idUserOwner);
-    } catch (error) {
-      throw error;
-    }
+    await verifyOwnership(req, id, "tour_guide");
   },
 };
 
 function getToken(authorization) {
   if (!authorization) {
-    throw error("No token", 401);
+    throw createError("No token", 401);
   }
 
   if (authorization.indexOf("Bearer") === -1) {
-    throw error("Formato Inválido", 401);
+    throw createError("Formato Inválido", 401);
   }
 
   let token = authorization.replace("Bearer ", "");
