3b2bcdc59716a719bb2e6889e48a486c04e3a242
Anthony Bernal Segura Angulo <absegura12@gmail.com>
2025-08-25 00:11:15 -0600

    chore: add flat ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module"
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true
+    },
+    rules: {}
+  }
+];
