86ec3564a48226bb16254872001d83aa3a05e1f4
Anthony Bernal Segura Angulo <absegura12@gmail.com>
2025-08-26 02:17:46 -0600

    Moving date test to tests folder

--- src/net/errors.js
+++ src/net/errors.js
@@ -2,7 +2,7 @@ import { error } from "./responses.js";
 import logger from "../utils/logger.js";
 
 export function errors(err, req, res, next) {
-  logger.error(err);
+  logger.error(err.message, {stack: err.stack});
 
   const status = err.statusCode || 500;
   const message =
--- tests/date.test.js
+++ tests/date.test.js
@@ -1,6 +1,6 @@
 import assert from 'assert';
-import { formatDate } from './date.js';
-import logger from './logger.js';
+import { formatDate } from '../src/utils/date.js';
+import logger from '../src/utils/logger.js';
 
 const date = new Date('2023-08-05');
 assert.strictEqual(formatDate(date), '2023-08-05');
