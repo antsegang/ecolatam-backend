50a5c0e180ccb645533fc065984fe222eb6af77a
Anthony Bernal Segura Angulo <absegura12@gmail.com>
2025-08-26 04:11:50 -0600

    fix: use UTC in formatDate to avoid timezone issues

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,6 +1,6 @@
 export function formatDate(date = new Date()) {
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const year = date.getFullYear();
+  const day = String(date.getUTCDate()).padStart(2, "0");
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+  const year = date.getUTCFullYear();
   return `${year}-${month}-${day}`;
 }
