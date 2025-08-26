66281d3ecce720ec54220acb6e463899c11e385a
Anthony Bernal Segura Angulo <absegura12@gmail.com>
2025-08-26 01:33:20 -0600

    Add error listeners in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import { app } from "./app.js";
 import logger from "./utils/logger.js";
 
+process.on("uncaughtException", (err) => {
+  logger.error("Uncaught Exception", err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason) => {
+  logger.error("Unhandled Rejection", reason);
+  process.exit(1);
+});
+
 app.listen(app.get("port"), () => {
   logger.info("Servidor ecsuchando en el puerto...", app.get("port"));
 });
